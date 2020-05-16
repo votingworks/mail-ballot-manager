9b1aa3cacd4ca1f2ff40efc647321aa8eab76075
Ben Adida <ben@adida.net>
2020-05-15 21:47:30 -0700

    heroku deployment scripts

---
+++ create.py
@@ -0,0 +1,19 @@
+from config import DATABASE_URL
+from server import db
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+if __name__ == "__main__":
+    engine = create_engine(DATABASE_URL)
+    print(f"database: {engine.url}")
+
+    try:
+        if not database_exists(engine.url):
+            print("creating database…")
+            create_database(engine.url)
+    except:
+        # sometimes, e.g. on Heroku, you can't even test if the database exists cause permissions
+        pass
+
+    print("creating tables…")
+    db.create_all()
