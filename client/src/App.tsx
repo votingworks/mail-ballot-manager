2dc639e0b522a9db5572c8fc6caf6062c7b92ba5
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-05-20 14:13:00 -0700

    feat: list ballot package contents
    This is really just meant as a way to illustrate using the `ballot-packages.ts` module.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback, useState } from 'react'
+import './App.css'
+import { readBallotPackage } from './util/ballot-package'
+import { Election } from '@votingworks/ballot-encoder'
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [election, setElection] = useState<Election>()
+  const [ballotNames, setBallotNames] = useState<string[]>()
+
+  const onInputChange = useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { files } = event.currentTarget
+
+      if (!files) {
+        return
+      }
+
+      for (const file of files) {
+        const ballotPackage = await readBallotPackage(file)
+
+        setElection(ballotPackage.election)
+        setBallotNames(
+          ballotPackage.ballots.map(
+            (ballot) => `${ballot.ballotStyle.id} / ${ballot.precinct.name}`
+          )
+        )
+      }
+    },
+    []
+  )
+
+  if (!election || !ballotNames) {
+    return <input type="file" ref={inputRef} onChange={onInputChange} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <h1>Election: {election.title}</h1>
+
+      <p>Ballots:</p>
+      <ul>
+        {ballotNames.map((ballotName) => (
+          <li>{ballotName}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
